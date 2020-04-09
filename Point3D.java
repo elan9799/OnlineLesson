bee5e36847f0e126491c12af0a28339eba09c78b
elan9799 <61521729+elan9799@users.noreply.github.com>
2020-04-09 21:53:41 +0300

    Update Point3D.java

--- Point3D.java
+++ Point3D.java
@@ -3,25 +3,33 @@ import java.util.Arrays;
 public class Point3D {
     public static void main(String[] args) {
 
-        points.getDistToZero(1 , 2 , 3);
-        points.getStringValue(1, 2 , 4);
-        points.gerDistToOtherPoint(1,2,3,8,2,1);
+        points a = new points(1, 5, 3);
+        a.getDistToZero();
+        a.getStringValue();
+        points b = new points(4,2,7);
+        points.gerDistToOtherPoint(a,b);
     }
 }
 class points{
     double x;
     double y;
     double z;
-    public static void getDistToZero (int x , int y, int z){
+    public points(double xx, double yy, double zz){
+        x = xx;
+        y = yy;
+        z = zz;
+    }
+
+    public void getDistToZero (){
         System.out.println("расстояние до центра = " + Math.sqrt(x*x + y*y + z*z));
 }
 
-    public static void getStringValue(int x, int y, int z) {
-        int array [] = {x,y,z};
+    public void getStringValue() {
+        double array [] = {x,y,z};
         System.out.println(Arrays.toString(array));
     }
 
-    public static void gerDistToOtherPoint(int x, int y, int z, int a, int b, int c) {
-        System.out.println(Math.sqrt((x - a) * (x - a) + (y - b) * (y - b) + (z - c) * (z - c)));
+    public static void gerDistToOtherPoint(points a, points b) {
+        System.out.println(Math.sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z)));
     }
 }
